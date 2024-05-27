7d9d4ee4f0de90430c6e66286feb7598cfdc5dd0
kimseungo <rtd7878@gmail.com>
2024-05-27 17:04:22 +0900

    init

---
+++ myBlog/app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express =require("express");
+const expressLayouts = require("express-ejs-layouts");
+const connectDb = require("./config/db");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+connectDb();
+
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("public"));
+
+app.use("/", require("./routes/main"));
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
---
+++ myBlog/routes/main.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const mainLayout = "../views/layouts/main.ejs";
+const Post = require("../models/Post");
+const asyncHandler = require("express-async-handler");
+
+router.get(["/", "/home"], asyncHandler(async(req, res) => {
+    const locals = {
+        title: "Home",
+    };
+    const data = await Post.find();
+    res.render("index", {locals, data, layout: mainLayout});
+}));
+router.get("/about", (req, res) => {
+    const locals = {
+        title: "about",
+    };
+    res.render("about", {locals, layout: mainLayout});
+});
+
+/**
+ * 게시물 상세 보기
+ * GET /post/:id
+ */
+router.get(
+    "/post/:id",
+    asyncHandler(async (req, res) => {
+        const data = await Post.findOne({ _id: req.params.id });
+        res.render("post", { data, layout: mainLayout});
+    })
+);
+
+
+module.exports =router;
+
+// Post.insertMany([
+//     {
+//         title: "제목 1",
+//         body: "내용 1 - 굿잡"
+//     },
+//     {
+//         title: "제목 2",
+//         body: "내용 2 - 굿잡"
+//     },
+//     {
+//         title: "제목 3",
+//         body: "내용 3 - 굿잡"
+//     },
+//     {
+//         title: "제목 4",
+//         body: "내용 4 - 굿잡"
+//     },
+//     {
+//         title: "제목 5",
+//         body: "내용 5 - 굿잡"
+//     },
+// ]);
